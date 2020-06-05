6e22adcdfcbbebe1eb6b6194ade2b0eb72a86158
Tulsi <tpj10@iitbbs.ac.in>
2020-06-05 20:25:50 +0530

    Added Second activity and corresponding layout for sending message

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -15,6 +16,13 @@ class MainActivity : AppCompatActivity() {
             //code
             Log.i("MainActivity", "Button was clicked")
             Toast.makeText(this,"Button was clicked", Toast.LENGTH_SHORT).show()
+        }
+        submitMessage.setOnClickListener{
+            val message: String = message.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
